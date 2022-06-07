186ba2cb0cfcabb4fb7d6f036e3d59002d37b5f9
FedeLopez17 <fedelopez1712@gmail.com>
2022-06-07 19:35:21 -0300

    Improve layout and add blur effect

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const hamburguerButton = document.querySelector(".hamburguer.button");
-const sidebar = document.querySelector(".sidebar")
+const sidebar = document.querySelector(".sidebar");
+const main = document.querySelector(".row.bottom.main");
+const navbar = document.querySelector(".row.top.navbar");
 hamburguerButton.addEventListener("click", toggleSidebar);
 
 function toggleSidebar(){
     sidebar.classList.toggle("show-sidebar");
     moveAndChangeHamburguer();
+    blur(main);
+    // The purpose of increasing the navbar's index is so that it doesn't reflect the blur of the element below. 
+    increaseIndex(navbar);
 }
 
 function moveAndChangeHamburguer(){
     hamburguerButton.classList.toggle("move-hamburguer");
     hamburguerButton.classList.toggle("change");
+}
+
+function blur(element){
+    element.classList.toggle("blur");
+}
+
+function increaseIndex(element){
+    element.classList.toggle("increase-index");
 }
