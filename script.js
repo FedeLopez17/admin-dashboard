1343bfe68c7ffbb15a277fe7e1bf9238cdb479a9
FedeLopez17 <fedelopez1712@gmail.com>
2022-06-09 13:52:17 -0300

    Styling and layout changes

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const hamburguerButton = document.querySelector(".hamburguer.button");
 const sidebar = document.querySelector(".sidebar");
+const sidebarElements = document.querySelectorAll(".sidebar > div");
 const main = document.querySelector(".row.bottom.main");
 const navbar = document.querySelector(".row.top.navbar");
+const navbarRight = document.querySelector(".navbar.right");
 hamburguerButton.addEventListener("click", toggleSidebar);
 
 function toggleSidebar(){
     sidebar.classList.toggle("show-sidebar");
+    console.log(sidebarElements);
+    for (let i = 0; i < 10; i++){
+         sidebarElements[i].classList.toggle("show-sidebar");
+     }
     moveAndChangeHamburguer();
     blur(main);
     // The purpose of increasing the navbar's index is so that it doesn't reflect the blur of the element below. 
     increaseIndex(navbar);
+    hide(navbarRight);
 }
 
 function moveAndChangeHamburguer(){
@@ -23,4 +30,8 @@ function blur(element){
 
 function increaseIndex(element){
     element.classList.toggle("increase-index");
+}
+
+function hide(element){
+    element.classList.toggle("hidden");
 }
